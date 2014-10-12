e72ac08c1237ed045b5e90ebc0f9362fc61bdbbf
Kelly McAlpine <eiramyllek73@gmail.com>
2014-10-12 00:18:33 -0400

    Added png images for GUI - edited js variable to match game theme

--- SlotMachine/js/main.js
+++ SlotMachine/js/main.js
@@ -1,15 +1,26 @@
 ﻿/// <reference path="jquery.js" />
 var playerMoney = 1000;
-var jackpot = 500;
+var winnings = 0;
+var jackpot = 5000;
 var turn = 0;
 var playerBet = 0;
-var previousBet = 0;
 var winNumber = 0;
 var lossNumber = 0;
 var spinResult;
-var fruits = "";
+var icons = "";
+var winRatio = 0;
+var reddit = 0;
+var html = 0;
+var notepad = 0;
+var css = 0;
+var linux = 0;
+var github = 0;
+var ie = 0;
+var blanks = 0;
+
+/* Utility function to show Player Stats */
 function showPlayerStats() {
-    var winRatio = winNumber / turn;
+    winRatio = winNumber / turn;
     $("#jackpot").text("Jackpot: " + jackpot);
     $("#playerMoney").text("Player Money: " + playerMoney);
     $("#playerTurn").text("Turn: " + turn);
@@ -17,6 +28,59 @@ function showPlayerStats() {
     $("#playerLosses").text("Losses: " + lossNumber);
     $("#playerWinRatio").text("Win Ratio: " + (winRatio * 100).toFixed(2) + "%");
 }
+
+/* Utility function to reset all icon tallies */
+function resetIconTally() {
+    reddit = 0;
+    html = 0;
+    notepad = 0;
+    css = 0;
+    linux = 0;
+    github = 0;
+    ie = 0;
+    blanks = 0;
+}
+
+/* Utility function to reset the player stats */
+function resetAll() {
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+}
+
+
+/* Check to see if the player won the jackpot */
+function checkJackPot() {
+    /* compare two random values */
+    var jackPotTry = Math.floor(Math.random() * 51 + 1);
+    var jackPotWin = Math.floor(Math.random() * 51 + 1);
+    if (jackPotTry == jackPotWin) {
+        alert("You Won the $" + jackpot + " Jackpot!!");
+        playerMoney += jackpot;
+        jackpot = 1000;
+    }
+}
+
+/* Utility function to show a win message and increase player money */
+function showWinMessage() {
+    playerMoney += winnings;
+    $("div#winOrLose>p").text("You Won: $" + winnings);
+    resetIconTally();
+    checkJackPot();
+}
+
+/* Utility function to show a loss message and reduce player money */
+function showLossMessage() {
+    playerMoney -= playerBet;
+    $("div#winOrLose>p").text("You Lost!");
+    resetIconTally();
+}
+
 /* Utility function to check if a value falls within a range of bounds */
 function checkRange(value, lowerBounds, upperBounds) {
     if (value >= lowerBounds && value <= upperBounds) {
@@ -26,50 +90,140 @@ function checkRange(value, lowerBounds, upperBounds) {
         return !value;
     }
 }
+
+/* When this function is called it determines the betLine results.
+e.g. Reddit - GitHub - CSS */
 function Reels() {
     var betLine = [" ", " ", " "];
     var outCome = [0, 0, 0];
+
     for (var spin = 0; spin < 3; spin++) {
         outCome[spin] = Math.floor((Math.random() * 65) + 1);
         switch (outCome[spin]) {
-            case checkRange(outCome[spin], 1, 26):
+            case checkRange(outCome[spin], 1, 27):  // 41.5% probability
                 betLine[spin] = "blank";
+                blanks++;
                 break;
-            case checkRange(outCome[spin], 27, 36):
-                betLine[spin] = "Grapes";
+            case checkRange(outCome[spin], 28, 37): // 15.4% probability
+                betLine[spin] = "Reddit";
+                reddit++;
                 break;
-            case checkRange(outCome[spin], 37, 45):
-                betLine[spin] = "Banana";
+            case checkRange(outCome[spin], 38, 46): // 13.8% probability
+                betLine[spin] = "HTML";
+                html++;
                 break;
-            case checkRange(outCome[spin], 46, 53):
-                betLine[spin] = "Orange";
+            case checkRange(outCome[spin], 47, 54): // 12.3% probability
+                betLine[spin] = "Notepad";
+                notepad++;
                 break;
-            case checkRange(outCome[spin], 54, 58):
-                betLine[spin] = "Cherry";
+            case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                betLine[spin] = "CSS";
+                css++;
                 break;
-            case checkRange(outCome[spin], 59, 61):
-                betLine[spin] = "Bar";
+            case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                betLine[spin] = "Linux";
+                linux++;
                 break;
-            case checkRange(outCome[spin], 62, 63):
-                betLine[spin] = "Bell";
+            case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                betLine[spin] = "GitHub";
+                github++;
                 break;
-            case checkRange(outCome[spin], 64, 65):
-                betLine[spin] = "Seven";
+            case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                betLine[spin] = "IE10";
+                ie++;
                 break;
         }
     }
     return betLine;
 }
-function pullTheHandle() {
-    /* Calculate win or loss */
+
+/* This function calculates the player's winnings, if any */
+function determineWinnings() {
+    if (blanks == 0) {
+        if (reddit == 3) {
+            winnings = playerBet * 10;
+        }
+        else if (html == 3) {
+            winnings = playerBet * 20;
+        }
+        else if (notepad == 3) {
+            winnings = playerBet * 30;
+        }
+        else if (css == 3) {
+            winnings = playerBet * 40;
+        }
+        else if (linux == 3) {
+            winnings = playerBet * 50;
+        }
+        else if (github == 3) {
+            winnings = playerBet * 75;
+        }
+        else if (ie == 3) {
+            winnings = playerBet * 100;
+        }
+        else if (reddit == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (html == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (notepad == 2) {
+            winnings = playerBet * 3;
+        }
+        else if (css == 2) {
+            winnings = playerBet * 4;
+        }
+        else if (linux == 2) {
+            winnings = playerBet * 5;
+        }
+        else if (github == 2) {
+            winnings = playerBet * 10;
+        }
+        else if (ie == 2) {
+            winnings = playerBet * 20;
+        }
+        else if (ie == 1) {
+            winnings = playerBet * 5;
+        }
+        else {
+            winnings = playerBet * 1;
+        }
+        winNumber++;
+        showWinMessage();
+    }
+    else {
+        lossNumber++;
+        showLossMessage();
+    }
+
 }
+
+/* When the player clicks the spin button the game kicks off */
 $("#spinButton").click(function () {
-    spinResult = Reels();
-    fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
-    $("div#result>p").text(fruits);
     playerBet = $("div#betEntry>input").val();
-    playerMoney -= playerBet;
-    pullTheHandle();
-    turn++;
-    showPlayerStats();
+
+    if (playerMoney == 0) {
+        if (confirm("You ran out of Money! \nDo you want to play again?")) {
+            resetAll();
+            showPlayerStats();
+        }
+    }
+    else if (playerBet > playerMoney) {
+        alert("You don't have enough Money to place that bet.");
+    }
+    else if (playerBet < 0) {
+        alert("All bets must be a positive $ amount.");
+    }
+    else if (playerBet <= playerMoney) {
+        spinResult = Reels();
+        icons = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+        $("div#result>p").text(icons);
+        determineWinnings();
+        turn++;
+        showPlayerStats();
+    }
+    else {
+        alert("Please enter a valid bet amount");
+    }
+
 });

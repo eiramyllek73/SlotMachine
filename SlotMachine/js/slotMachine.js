66fdac82cbea370e011a28968d4d9cc4c120e883
Kelly McAlpine <eiramyllek73@gmail.com>
2014-10-31 20:24:44 -0400

    Added createJS libraries, icons, revamped PNG's, edited index

---
+++ SlotMachine/js/slotMachine.js
@@ -0,0 +1,14 @@
+﻿window.onload = function () {
+    var slotCanvas = document.getElementById('slotCanvas');
+    var ctx = slotCanvas.getContext("2d");
+    slotCanvas.width = 800;
+    slotCanvas.height = 800;
+
+    function drawSlotMachine()
+    {
+        var imgPath = "images/slotmachine1/png";
+        slotMachine = new createJS.Bitmap(imgPath);
+        stage.addChild(slotMachine);
+        stage.update();
+    }
+}

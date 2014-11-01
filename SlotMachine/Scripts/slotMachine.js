0d79a1d9db1ce8849924756d2ffde96cad5cdcfe
Kelly McAlpine <eiramyllek73@gmail.com>
2014-11-01 06:19:18 -0400

    Add additional styling to css

--- SlotMachine/Scripts/slotMachine.js
+++ SlotMachine/Scripts/slotMachine.js
@@ -101,40 +101,6 @@ function tick(e) {
     stage.update();
 }
 
-/* Loader Bar functions from "Beginning HTML5 Games with CreateJS" pp26-28 */
-function buildLoaderBar() {
-    loaderBar = new createjs.Shape();
-    loaderBar.x = loaderBar.y = 100;
-    loaderBar.graphics.setStrokeStyle(2);
-    loaderBar.graphics.beginStroke("#000");
-    loaderBar.graphics.drawRect(0, 0, LOADER_WIDTH, 40);
-    stage.addChild(loaderBar);
-}
-
-function updateLoaderBar() {
-    loaderBar.graphics.clear();
-    loaderBar.graphics.beginFill('#00ff00');
-    loaderBar.graphics.drawRect(0, 0, LOADER_WIDTH * percentLoaded, 40);
-    loaderBar.graphics.endFill();
-    loaderBar.graphics.setStrokeStyle(2);
-    loaderBar.graphics.beginStroke("#000");
-    loaderBar.graphics.drawRect(0, 0, LOADER_WIDTH, 40);
-    loaderBar.graphics.endStroke();
-}
-
-function startLoad() {
-    loadInterval = setInterval(updateLoad, 50);
-}
-function updateLoad() {
-    percentLoaded += .005;
-    updateLoaderBar();
-    if (percentLoaded >= 1) {
-        clearInterval(loadInterval);
-        stage.removeChild(loaderBar);
-    }
-
-    buildSlotMachine();
-}
 /* Preload function for game */
 function preload()
 {
@@ -175,12 +141,12 @@ function preload()
             {id:  "youLose", src: "sounds/youlose.wav" },
             {id:  "youWon", src: "sounds/youWon.wav" }
         ]);
-
 }
 
 function buildSlotMachine()
  {
     //Slot-Machine background
+    slotGame = new createjs.Container();
     createjs.Sound.play('welcome', createjs.Sound.INTERRUPT_NONE);
 
     slotMachineBase = new createjs.Bitmap(queue.getResult('slotMachineBase'));
@@ -236,16 +202,34 @@ function buildSlotMachine()
     spinButton = new createjs.Bitmap(queue.getResult('spinButton'));
     spinButton.x = 438;
     spinButton.y = 655;
+    spinButtonHover = new createjs.Bitmap(queue.getResult('spinButtonH'));
+    spinButtonHover.x = 438;
+    spinButtonHover.y = 655;
 
-    resetButton = new createjs.Bitmap(queue.getResult('restButton'));
+    resetButton = new createjs.Bitmap(queue.getResult('resetButton'));
     resetButton.x = 615;
     resetButton.y = 655;
+    resetButtonHover = new createjs.Bitmap(queue.getResult('resetButtonH'));
+    resetButtonH.x = 615;
+    resetButtonH.y = 655;
 
     exitButton = new createjs.Bitmap(queue.getResult('exitButton'));
     exitButton.x = 782;
     exitButton.y = 655;
+    
 
-    spinButton.addEventListener("click", spinReels);
+    spinButton.addEventListener("mouseover", function (event)
+    {
+        spinButton.visible = false;
+        spinButtonHover.visible = true;
+        if(!playerBet)
+        {
+            spinButtonHover.alpha = 0.5;
+            $('slotCanvas')
+        }
+    }
+    
+    );
     resetButton.addEventListener("click", resetGame);
     exitButton.addEventListener("click", endGame);
 
@@ -262,13 +246,146 @@ function buildSlotMachine()
     blank3.x = 679;
     blank3.y = 298;
 
-    stage.addChild(slotMachineBase, creditsWindow, betWindow, paidWindow, bet1Button, bet10Button, bet5Button, bet50Button, bet100Button, bet500Button, spinButton, resetButton, exitButton, blank, blank2, blank3);
+    stage.addChild(slotMachineBase, creditsWindow, betWindow, paidWindow, bet1Button, bet10Button, bet5Button, bet50Button, bet100Button, bet500Button, spinButton, resetButton, exitButton, spinButtonH, resetButtonH, exitButtonH, blank, blank2, blank3);
 
     showPlayerStats();
-    stage.update();
+}
+
+/* Loader Bar functions from "Beginning HTML5 Games with CreateJS" pp26-28 */
+function buildLoaderBar()
+{
+    loaderBar = new createjs.Shape();
+    loaderBar.x = loaderBar.y = 100;
+    loaderBar.graphics.setStrokeStyle(2);
+    loaderBar.graphics.beginStroke("#000");
+    loaderBar.graphics.drawRect(0, 0, LOADER_WIDTH, 40);
+    stage.addChild(loaderBar);
+}
+
+function updateLoaderBar()
+{
+    loaderBar.graphics.clear();
+    loaderBar.graphics.beginFill('#00ff00');
+    loaderBar.graphics.drawRect(0, 0, LOADER_WIDTH * percentLoaded, 40);
+    loaderBar.graphics.endFill();
+    loaderBar.graphics.setStrokeStyle(2);
+    loaderBar.graphics.beginStroke("#000");
+    loaderBar.graphics.drawRect(0, 0, LOADER_WIDTH, 40);
+    loaderBar.graphics.endStroke();
+}
+
+function startLoad()
+{
+    loadInterval = setInterval(updateLoad, 50);
+}
+function updateLoad()
+{
+    percentLoaded += .005;
+    updateLoaderBar();
+    if (percentLoaded >= 1)
+    {
+        clearInterval(loadInterval);
+        stage.removeChild(loaderBar);
+    }
+
 }
 
 
+/* Utility function to spin the reels only if play has enough money to place bet (check) */
+function spinreels()
+{
+    //check if the player has enough money to place bet/spin reels
+    if (playerMoney < 1)
+    {
+        endGame();
+    }
+
+    //Let Player know his/her bet exceeds balance
+    else if (playerBet > playerMoney)
+    {
+        alert("Apologies...You do not have enough funds to cover that bet.");
+    }
+
+    //If bet is good, continue
+    else if (playerBet <= playerMoney)
+    {
+        createjs.Sound.play("spin");
+        playerMoney -= playerBet;
+
+        Reels();
+    }
+}
+/* THE FOLLOWING CODE WAS PROVIDED BY TOM!! https://github.com/tsiliopoulos/ */
+/* When this function is called it determines the betLine results.
+e.g. Reddit - GitHub - CSS */
+function Reels()
+{
+    var result;
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+
+    for (var spin = 0; spin < 3; spin++)
+    {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin])
+        {
+            case checkRange(outCome[spin], 1, 27):  // 41.5% probability
+                betLine[spin] = "blank";
+                blanks++;
+                break;
+            case checkRange(outCome[spin], 28, 37): // 15.4% probability
+                betLine[spin] = "reddit";
+                reddit++;
+                break;
+            case checkRange(outCome[spin], 38, 46): // 13.8% probability
+                betLine[spin] = "html";
+                html++;
+                break;
+            case checkRange(outCome[spin], 47, 54): // 12.3% probability
+                betLine[spin] = "notepad";
+                notepad++;
+                break;
+            case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                betLine[spin] = "css";
+                css++;
+                break;
+            case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                betLine[spin] = "linux";
+                linux++;
+                break;
+            case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                betLine[spin] = "gitHub";
+                github++;
+                break;
+            case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                betLine[spin] = "ie";
+                ie++;
+                break;
+        }
+
+        //display spin results
+        reel1 = result;
+        reel1.x = 169;
+        reel1.y = 300;
+        slotGame.addChild(reel1);
+
+        reel2 = result;
+        reel2.x = 426;
+        reel2.y = 312;
+        slotGame.addChild(reel2);
+
+        reel3 = result;
+        reel3.x = 679;
+        reel3.y = 298;
+        slotGame.addChild(reel3);
+
+        updateStats();
+
+        stage.update();
+    }
+
+    return betLine;
+}
 /* Utility function to show Player Stats */
 function showPlayerStats() {
     winRatio = winNumber / turn;
@@ -295,7 +412,8 @@ function resetBetButtons()
 
 
 /* Utility function to reset all icon tallies */
-function resetIconTally() {
+function resetIconTally()
+{
     reddit = 0;
     html = 0;
     notepad = 0;
@@ -398,107 +516,81 @@ function checkRange(value, lowerBounds, upperBounds) {
     }
 }
 
-/* When this function is called it determines the betLine results.
-e.g. Reddit - GitHub - CSS */
-function Reels() {
-    var betLine = [" ", " ", " "];
-    var outCome = [0, 0, 0];
 
-    for (var spin = 0; spin < 3; spin++) {
-        outCome[spin] = Math.floor((Math.random() * 65) + 1);
-        switch (outCome[spin]) {
-            case checkRange(outCome[spin], 1, 27):  // 41.5% probability
-                betLine[spin] = "blank";
-                blanks++;
-                break;
-            case checkRange(outCome[spin], 28, 37): // 15.4% probability
-                betLine[spin] = "Reddit";
-                reddit++;
-                break;
-            case checkRange(outCome[spin], 38, 46): // 13.8% probability
-                betLine[spin] = "HTML";
-                html++;
-                break;
-            case checkRange(outCome[spin], 47, 54): // 12.3% probability
-                betLine[spin] = "Notepad";
-                notepad++;
-                break;
-            case checkRange(outCome[spin], 55, 59): //  7.7% probability
-                betLine[spin] = "CSS";
-                css++;
-                break;
-            case checkRange(outCome[spin], 60, 62): //  4.6% probability
-                betLine[spin] = "Linux";
-                linux++;
-                break;
-            case checkRange(outCome[spin], 63, 64): //  3.1% probability
-                betLine[spin] = "GitHub";
-                github++;
-                break;
-            case checkRange(outCome[spin], 65, 65): //  1.5% probability
-                betLine[spin] = "IE10";
-                ie++;
-                break;
-        }
-    }
-    return betLine;
-}
 
 /* This function calculates the player's winnings, if any */
-function determineWinnings() {
-    if (blanks == 0) {
-        if (reddit == 3) {
+function determineWinnings()
+{
+    if (blanks == 0)
+    {
+        if (reddit == 3)
+        {
             winnings = playerBet * 10;
         }
         else if (html == 3) {
             winnings = playerBet * 20;
         }
-        else if (notepad == 3) {
+        else if (notepad == 3)
+        {
             winnings = playerBet * 30;
         }
-        else if (css == 3) {
+        else if (css == 3)
+        {
             winnings = playerBet * 40;
         }
-        else if (linux == 3) {
+        else if (linux == 3)
+        {
             winnings = playerBet * 50;
         }
-        else if (github == 3) {
+        else if (github == 3)
+        {
             winnings = playerBet * 75;
         }
-        else if (ie == 3) {
+        else if (ie == 3)
+        {
             winnings = playerBet * 100;
         }
-        else if (reddit == 2) {
+        else if (reddit == 2)
+        {
             winnings = playerBet * 2;
         }
-        else if (html == 2) {
+        else if (html == 2)
+        {
             winnings = playerBet * 2;
         }
-        else if (notepad == 2) {
+        else if (notepad == 2)
+        {
             winnings = playerBet * 3;
         }
-        else if (css == 2) {
+        else if (css == 2)
+        {
             winnings = playerBet * 4;
         }
-        else if (linux == 2) {
+        else if (linux == 2)
+        {
             winnings = playerBet * 5;
         }
-        else if (github == 2) {
+        else if (github == 2)
+        {
             winnings = playerBet * 10;
         }
-        else if (ie == 2) {
+        else if (ie == 2)
+        {
             winnings = playerBet * 20;
         }
-        else if (ie == 1) {
+        else if (ie == 1)
+        {
             winnings = playerBet * 5;
         }
-        else {
+        else
+        {
             winnings = playerBet * 1;
         }
         winNumber++;
         showWinMessage();
     }
-    else {
+    else
+    {
         lossNumber++;
         showLossMessage();
     }
@@ -506,22 +598,28 @@ function determineWinnings() {
 }
 
 /* When the player clicks the spin button the game kicks off */
-$("#spinButton").click(function () {
+$("#spinButton").click(function ()
+{
     playerBet = $("div#betEntry>input").val();
 
-    if (playerMoney == 0) {
-        if (confirm("You ran out of Money! \nDo you want to play again?")) {
+    if (playerMoney == 0)
+    {
+        if (confirm("You ran out of Money! \nDo you want to play again?"))
+        {
             resetAll();
             showPlayerStats();
         }
     }
-    else if (playerBet > playerMoney) {
-        alert("You don't have enough Money to place that bet.");
+    else if (playerBet > playerMoney)
+    {
+        alert("Apologies...You do not have enough funds to cover that bet.");
     }
-    else if (playerBet < 0) {
+    else if (playerBet < 0)
+    {
         alert("All bets must be a positive $ amount.");
     }
-    else if (playerBet <= playerMoney) {
+    else if (playerBet <= playerMoney)
+    {
         spinResult = Reels();
         icons = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
         $("div#result>p").text(icons);
@@ -529,7 +627,8 @@ $("#spinButton").click(function () {
         turn++;
         showPlayerStats();
     }
-    else {
+    else
+    {
         alert("Please enter a valid bet amount");
     }
 
